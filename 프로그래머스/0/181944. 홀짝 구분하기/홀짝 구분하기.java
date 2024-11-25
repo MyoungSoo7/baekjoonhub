75c618677b20278a54cc47e9541c677b675c3387
LIM MYOUNGSOO <iamipro@naver.com>
2024-11-25 21:38:29 +0900

    [level 0] Title: 홀짝 구분하기, Time: 184.91 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -4,11 +4,9 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int n = sc.nextInt();
-        if(n % 2 ==0){
-            System.out.println(n+ " is even");
-        }else{
-            System.out.println(n+ " is odd");
-        }
+   
+        System.out.println(n+ " is "+ (n %2 ==0 ? "even": "odd") );
+        
         
         
         
